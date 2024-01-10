02981df0bbd2f36721cae423a7b81e2fa919837a
bradenmweight <braden.m.weight@gmail.com>
2024-01-10 13:21:43 -0500

    Updated

--- Polariton_PF_1Subspace/run.sh
+++ Polariton_PF_1Subspace/run.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-for a in {0..20..1}; do
-    A0=$(echo "$a*0.01" | bc -l)
-    echo $A0
-    sbatch submit.RI 1 ${A0}
-    sbatch submit.RI 2 ${A0}
-    sbatch submit.RI 3 ${A0}
-    sbatch submit.RI 5 ${A0}
-    sbatch submit.RI 10 ${A0}
-    sbatch submit.RI 100 ${A0}
-    sbatch submit.RI 1000 ${A0}
-    sbatch submit.RI 10000 ${A0}
-    sbatch submit.RI 100000 ${A0}
-    sbatch submit.RI 1000000 ${A0}
+for wci in {50..150..5}; do
+    WC=$(echo "$wci*0.01" | bc -l)
+    echo $WC
+    sbatch submit.RI 10000 0.3 ${WC}
 done
 
